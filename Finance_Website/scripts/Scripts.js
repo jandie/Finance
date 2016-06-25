247c95e3a90132d8c378ed15836427b81e62695f
jandie <eidnaj@live.nl>
2016-06-25 19:33:19 +0200

    Minor UI improvements with javascript

---
+++ Finance_Website/scripts/Scripts.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $("#AddBalanceModal").on("shown.bs.modal", function () {
+        $("#name").focus();
+    });
+    $("#AddMonthlyBill").on("shown.bs.modal", function () {
+        $("#billName").focus();
+    });
+    $("#AddMonthlyIncome").on("shown.bs.modal", function () {
+        $("#incomeName").focus();
+    });
+});
