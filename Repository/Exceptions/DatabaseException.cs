81a5dc12f37dcfa1d1dcd507a1615fa8588be8ba
jandie <eidnaj@live.nl>
2016-06-25 15:58:32 +0200

    DatabaseException added

---
+++ Repository/Exceptions/DatabaseException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Repository.Exceptions
+{
+    public class DatabaseException : Exception
+    {
+        public DatabaseException()
+        {
+            
+        }
+
+        public DatabaseException(string message) : base(message)
+        {
+            
+        }
+    }
+}
