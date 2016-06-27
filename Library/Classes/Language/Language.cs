dd9dc903f284579e28366bf2db54782d86ef177b
jandie <eidnaj@live.nl>
2016-06-27 16:03:12 +0200

    Language classes added to library

---
+++ Library/Classes/Language/Language.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Library.Classes.Language
+{
+    public class Language
+    {
+        private List<Translation> _translations;
+
+        public Language(int id, string abbrevation, string name)
+        {
+            Id = id;
+            Abbrevation = abbrevation;
+            Name = name;
+
+            _translations = new List<Translation>();
+        }
+
+        public int Id { get; }
+
+        public string Abbrevation { get; }
+
+        public string Name { get; }
+
+        public List<Translation> Translations => new List<Translation>(_translations);
+
+        public void AddTranslation(Translation translation)
+        {
+            _translations.Add(translation);
+        }
+    }
+}
---
+++ Library/Classes/Language/Translation.cs
@@ -0,0 +1,15 @@
+﻿namespace Library.Classes.Language
+{
+    public class Translation
+    {
+        public Translation(int id, string translationText)
+        {
+            Id = id;
+            TranslationText = translationText;
+        }
+
+        public int Id { get; }
+        
+        public string TranslationText { get; }
+    }
+}
