ec0e53c394e26b1b760600a4ce3b07b71c22311a
jandie <eidnaj@live.nl>
2016-06-29 15:58:41 +0200

    Currency class added and language stored in user

---
+++ Library/Classes/Currency.cs
@@ -0,0 +1,21 @@
+﻿namespace Library.Classes
+{
+    public class Currency
+    {
+        public Currency(int id, string abbrevation, string name, string html)
+        {
+            Id = id;
+            Abbrevation = abbrevation;
+            Name = name;
+            Html = html;
+        }
+
+        public int Id { get; }
+
+        public string Abbrevation { get; }
+
+        public string Name { get; }
+
+        public string Html { get; }
+    }
+}
--- Library/Classes/User.cs
+++ Library/Classes/User.cs
@@ -9,12 +9,14 @@ namespace Library.Classes
 
         private List<IPayment> _payments;
 
-        public User(int id, string name, string lastName, string email)
+        public User(int id, string name, string lastName, string email, int languageId, Currency currency)
         {
             Id = id;
             Name = name;
             LastName = lastName;
             Email = email;
+            LanguageId = languageId;
+            Currency = currency;
 
             _balances = new List<Balance>();
             _payments = new List<IPayment>();
@@ -24,13 +26,17 @@ namespace Library.Classes
 
         public List<IPayment> Payments => new List<IPayment>(_payments);
 
-        public int Id { get; set; }
+        public int Id { get; }
 
-        public string Email { get; set; }
+        public string Name { get; }
 
-        public string Name { get; set; }
+        public string LastName { get; }
 
-        public string LastName { get; set; }
+        public string Email { get; }
+
+        public int LanguageId { get; }
+
+        public Currency Currency { get; }
 
         public decimal Prediction
         {
