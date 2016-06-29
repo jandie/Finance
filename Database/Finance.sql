d87632217df719c55d1037a264e67edd1de79bd6
jandie <eidnaj@live.nl>
2016-06-29 15:28:59 +0200

    Added new foreign keys to user

--- Database/Finance.sql
+++ Database/Finance.sql
@@ -6,14 +6,41 @@ DROP TABLE Translation;
 DROP TABLE Language;
 DROP TABLE Currency;
 
-CREATE TABLE User (
-    Id INT NOT NULL AUTO_INCREMENT,
-    Name VARCHAR(255),
-    LastName VARCHAR(255),
-    Email VARCHAR(255) UNIQUE,
-    Password VARCHAR(255),
-    Active INT DEFAULT 1,
+CREATE TABLE Language (
+    Id INT NOT NULL,
+    Abbrevation VARCHAR(3) NOT NULL,
+    Name VARCHAR(255) NOT NULL,
     PRIMARY KEY (Id)
+);
+
+CREATE TABLE Translation (
+    Id INT NOT NULL,
+    Language_Id INT NOT NULL,
+    Translation VARCHAR(255) NOT NULL,
+    PRIMARY KEY (Id , Language_Id),
+    FOREIGN KEY (Language_Id)
+        REFERENCES Language (Id)
+);
+
+CREATE TABLE Currency (
+    Id INT NOT NULL AUTO_INCREMENT,
+    Abbrevation VARCHAR(3) NOT NULL,
+    Name VARCHAR(255) NOT NULL,
+    Html VARCHAR(255) NOT NULL,
+    PRIMARY KEY (Id)
+);
+
+CREATE TABLE `user` (
+    `Id` INT(11) NOT NULL AUTO_INCREMENT,
+    `Name` VARCHAR(255) NOT NULL,
+    `LastName` VARCHAR(255) NOT NULL,
+    `Email` VARCHAR(255) DEFAULT NULL,
+    `Password` VARCHAR(255) NOT NULL,
+    `Active` INT(11) DEFAULT '1',
+    `Currency` INT(11) DEFAULT '0',
+    `Language` INT(11) DEFAULT '0',
+    PRIMARY KEY (`Id`),
+    UNIQUE KEY `Email` (`Email`)
 );
 
 CREATE TABLE BankAccount (
@@ -49,30 +76,6 @@ CREATE TABLE Transaction (
     PRIMARY KEY (Id),
     FOREIGN KEY (Payment_Id)
         REFERENCES Payment (Id)
-);
-
-CREATE TABLE Language (
-    Id INT NOT NULL,
-    Abbrevation VARCHAR(3) NOT NULL,
-    Name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (Id)
-);
-
-CREATE TABLE Translation (
-    Id INT NOT NULL,
-    Language_Id INT NOT NULL,
-    Translation VARCHAR(255) NOT NULL,
-    PRIMARY KEY (Id, Language_Id),
-    FOREIGN KEY (Language_Id)
-        REFERENCES Language (Id)
-);
-
-CREATE TABLE Currency (
-    Id INT NOT NULL AUTO_INCREMENT,
-    Abbrevation VARCHAR(3) NOT NULL,
-    Name VARCHAR(255) NOT NULL,
-    Html VARCHAR(255) NOT NULL,
-    PRIMARY KEY (Id)
 );
 
 INSERT INTO USER (NAME, LASTNAME, EMAIL, PASSWORD) VALUES ('Jandie', 'Hendriks', 'jandie@live.nl', 'test');
