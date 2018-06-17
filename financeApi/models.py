aed4f2ba56c02837081d6ff922068df823bba857
jandie <eidnaj@live.nl>
2018-06-17 10:54:49 +0200

    Base models added and migrated

--- financeApi/models.py
+++ financeApi/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Balance(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    outgoing = models.BooleanField(default=True)
+
+
+class Transaction(models.Model):
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
+    description = models.CharField(max_length=1024)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
