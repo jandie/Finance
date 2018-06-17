223f578ef6e4aec94d149c439b95cd69dc8d6713
jandie <eidnaj@live.nl>
2018-06-17 15:45:08 +0200

    Admin url changed

--- finance/urls.py
+++ finance/urls.py
@@ -19,6 +19,6 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls')),
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     url(r'^', include('financeApi.urls')),
 ]
