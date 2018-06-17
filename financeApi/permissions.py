49ee21b4317cd1b01fa236c968e62379033360bf
jandie <eidnaj@live.nl>
2018-06-17 14:33:58 +0200

    Balances implemented

--- financeApi/permissions.py
+++ financeApi/permissions.py
@@ -5,7 +5,14 @@ class IsPaymentOwner(permissions.BasePermission):
     """
     Custom permission to only allow owners of a payment to get, edit or delete it.
     """
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
 
+
+class IsBalanceOwner(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of a payment to get, edit or delete it.
+    """
     def has_object_permission(self, request, view, obj):
         print(obj.user)
         print(request.user)
--- financeApi/urls.py
+++ financeApi/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
 
-from financeApi.views import users, payments
+from financeApi.views import users, payments, balances
 
 urlpatterns = [
     url(r'^api-token-auth/', obtain_jwt_token),
@@ -11,6 +11,8 @@ urlpatterns = [
     url(r'^users/$', users.users_creation),
     url(r'^payments/$', payments.PaymentList.as_view()),
     url(r'^payments/(?P<pk>[0-9]+)/$', payments.PaymentDetail.as_view()),
+    url(r'^balances/$', balances.BalanceList.as_view()),
+    url(r'^balances/(?P<pk>[0-9]+)/$', balances.BalanceDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
