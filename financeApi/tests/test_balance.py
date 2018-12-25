2fddf1bd1a2e231bfbce3d48afcac289888f5d0a
jandie <eidnaj@live.nl>
2018-12-25 14:30:54 +0100

    Balance authorizations tests

--- financeApi/tests/test_balance.py
+++ financeApi/tests/test_balance.py
@@ -15,6 +15,35 @@ class BalanceTests(TestCase):
         self.user2 = None
         create_user(self)
 
+    def test_get_balance(self):
+        balance = self.user.balance_set.create(name='testing balance',
+                                               amount=563.93)
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='JWT ' + self.token)
+        url = 'http://testserver/balances/{}/'.format(balance.id)
+        response = client.get(url, content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['name'], 'testing balance')
+        self.assertEqual(response.data['amount'], '563.93')
+
+    def test_get_balances(self):
+        self.user.balance_set.create(name='testing balance',
+                                               amount=563.93)
+        self.user.balance_set.create(name='testing balance2',
+                                               amount=563.03)
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='JWT ' + self.token)
+        url = 'http://testserver/balances/'
+        response = client.get(url, content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data[0]['name'], 'testing balance')
+        self.assertEqual(response.data[0]['amount'], '563.93')
+        self.assertEqual(response.data[1]['name'], 'testing balance2')
+        self.assertEqual(response.data[1]['amount'], '563.03')
+        self.assertEqual(len(response.data), 2)
+
     def test_create_balance(self):
         factory = APIRequestFactory()
         view = BalanceList.as_view()
@@ -82,3 +111,27 @@ class BalanceTests(TestCase):
         self.assertEqual(response.data['name'], 'test balance34')
         self.assertEqual(response.data['amount'], '108.36')
 
+    def test_balance_security(self):
+        balance = self.user2.balance_set.create(name='testing balance',
+                                               amount=563.93)
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='JWT ' + self.token)
+        url = 'http://testserver/balances/{}/'.format(balance.id)
+        response = client.get(url, content_type='application/json')
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_get_balances_security(self):
+        self.user.balance_set.create(name='testing balance',
+                                               amount=563.93)
+        self.user2.balance_set.create(name='testing balance2',
+                                               amount=563.03)
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='JWT ' + self.token)
+        url = 'http://testserver/balances/'
+        response = client.get(url, content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data[0]['name'], 'testing balance')
+        self.assertEqual(response.data[0]['amount'], '563.93')
+        self.assertEqual(len(response.data), 1)
